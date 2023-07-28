5df5398a8f175cc897b55285a49f675617397f53
whx <453212683@qq.com>
2023-07-28 20:48:34 +0800

    Added      XFrameworkBase/Code/Resource/CResourceMgr.ResourceLoader.LoadAssetTaskBase.cs

---
+++ XFrameworkBase/Code/Resource/CResourceMgr.ResourceLoader.LoadAssetTaskBase.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace XFrameworkBase
+{
+    public partial class CResourceMgr : CGameframeworkMoudle
+    {
+        private sealed partial class CResourceLoader
+        {
+            private class CLoadAssetTaskBase : CTaskBase
+            {
+                private CResourceObject m_refResourceObj;
+                private CAssetObject m_AssetObj;
+                private CAssetInfo m_refAssetInfo;
+                private List<object> m_listDependAsset;
+
+                public CLoadAssetTaskBase()
+                {
+                    m_listDependAsset = new List<object>();
+                }
+                public override void Clear()
+                {
+                    base.Clear();
+                    m_refResourceObj = null;
+                    m_AssetObj = null;
+                    m_refAssetInfo = null;
+                    m_listDependAsset.Clear();
+                }
+            }
+        }
+    }
+}
